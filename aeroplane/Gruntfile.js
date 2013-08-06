b08419d2a417b21d1804ecdd29b363207226b571
Bernardo Heynemann <heynemann@gmail.com>
2013-08-06 09:44:47 -0300

    Aeroplane started

--- aeroplane/Gruntfile.js
+++ aeroplane/Gruntfile.js
@@ -37,6 +37,10 @@ module.exports = function (grunt) {
         files: ['test/spec/{,*/}*.coffee'],
         tasks: ['coffee:test']
       },
+      compass: {
+        files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
+        tasks: ['compass:server']
+      },
       livereload: {
         options: {
           livereload: LIVERELOAD_PORT
@@ -131,6 +135,27 @@ module.exports = function (grunt) {
           dest: '.tmp/spec',
           ext: '.js'
         }]
+      }
+    },
+    compass: {
+      options: {
+        sassDir: '<%= yeoman.app %>/styles',
+        cssDir: '.tmp/styles',
+        generatedImagesDir: '.tmp/images/generated',
+        imagesDir: '<%= yeoman.app %>/images',
+        javascriptsDir: '<%= yeoman.app %>/scripts',
+        fontsDir: '<%= yeoman.app %>/styles/fonts',
+        importPath: '<%= yeoman.app %>/bower_components',
+        httpImagesPath: '/images',
+        httpGeneratedImagesPath: '/images/generated',
+        httpFontsPath: '/styles/fonts',
+        relativeAssets: false
+      },
+      dist: {},
+      server: {
+        options: {
+          debugInfo: true
+        }
       }
     },
     // not used since Uglify task does concat,
@@ -244,13 +269,16 @@ module.exports = function (grunt) {
     },
     concurrent: {
       server: [
-        'coffee:dist'
+        'coffee:dist',
+        'compass:server'
       ],
       test: [
-        'coffee'
+        'coffee',
+        'compass'
       ],
       dist: [
         'coffee',
+        'compass:dist',
         'imagemin',
         'svgmin',
         'htmlmin'
